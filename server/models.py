a616df2d58a8ddbe44ce2ec2a5434c95e974a748
kevin <kevinke386@gmail.com>
2024-08-01 15:16:10 +0300

    Updated

---
+++ server/models.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from flask_bcrypt import Bcrypt, check_password_hash
+from sqlalchemy import DateTime, ForeignKey, Numeric
+from sqlalchemy.orm import relationship
+from sqlalchemy_serializer import SerializerMixin
+from config import db
+
+bcrypt = Bcrypt()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email= db.Column(db.String)
+    password = db.Column(db.String)
+    role = db.column(db.String)
+
+    def __repr__(self):
+        return f'<User {self.id}, {self.name}, {self.email}, {self.password}, {self.role}>'
+
