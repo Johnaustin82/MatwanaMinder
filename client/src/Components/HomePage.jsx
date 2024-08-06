b80f38ac8e11512c83f80524cbf7ce9432edd188
saruni <timothysaruni13@gmail.com>
2024-08-06 15:02:46 +0300

    adds landing page

---
+++ client/src/Components/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './HomePage.css'
+function HomePage() {
+    return (
+      <div className="home-page">
+        <div className="content">
+          <p>Ride smart,manage better,your ultimate matatu solution.</p>
+        </div>
+        <div className="image-container">
+          <img src= "https://i.pinimg.com/564x/34/3a/f4/343af4286a85b9a1765fd3df712b6a48.jpg"alt="Side" />
+        </div>
+      </div>
+    );
+  }
+  
+  export default HomePage;
