22f6b552bb48d3b92ee7b53bf435265976719b03
Felicity <felicitymuhonja33@gmail.com>
2024-08-07 14:17:28 +0300

    Correction

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import OurServices from './components/OurServices.jsx';
-import Fleet from './components/Fleet.jsx';
 
 
 function App (){
 
   return (
-    <>
-    <OurServices/>
-    <Fleet/>
-    </>
+    <></>
   )
 }
 
