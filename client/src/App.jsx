6e0bacd6afa7e81aff815bb18c40aaa8166d8b5c
mahad <muhumedmohamed836@gmail.com>
2024-08-10 22:06:34 +0300

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/LoginForm';
-import Sign from './components/SignupForm';
+
 
 function App() {
   return (
-    <Router>
       <div className="App">
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/sign" element={<Sign />} />
-          {/* Add a default route or redirect if needed */}
-          <Route path="/" element={<Login />} /> {/* Default route */}
-        </Routes>
+      
       </div>
-    </Router>
   );
 }
 
