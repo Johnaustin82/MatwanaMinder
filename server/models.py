32245875c5ac598f7cb3a6ffda4d980f00cf5393
kevin <kevinke386@gmail.com>
2024-08-11 21:54:52 +0300

    Updated

--- server/models.py
+++ server/models.py
@@ -32,8 +32,8 @@ class User(db.Model):
 
 class Vehicle(db.Model):
     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
-    license_plate = db.Column(db.String(120), nullable=False)
-    model = db.Column(db.String(120), nullable=False)
+    license_plate = db.Column(db.String(255),unique=True, nullable=False)
+    model = db.Column(db.String(255), nullable=False)
     capacity = db.Column(db.Integer, nullable=False)
     operator_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     start_time = db.Column(db.Time, nullable=False)
