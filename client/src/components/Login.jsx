93fe7fd7b00e13b4d544b7c13f24013e661d5e9b
John Austin Ngode <austinngode23@gmail.com>
2024-08-19 18:32:38 +0300

    Updates login endpoint

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -19,7 +19,7 @@ const LoginForm = () => {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:5000/login", {
+      const response = await fetch("https://matwanaminder-7.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
